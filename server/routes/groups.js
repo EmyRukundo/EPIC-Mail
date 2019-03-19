9da5052f587ee2ff6940ff2ae7a57238b6439892
EmyRukundo <rukundoemma@gmail.com>
2019-03-19 20:49:20 +0200

    [Feature #164740438] Add update name

--- server/routes/groups.js
+++ server/routes/groups.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import {getGroups,createGroup} from '../controllers/groups';
+import {getGroups,createGroup,updateGroup,specificGroup} from '../controllers/groups';
 const router = express.Router();
 
 
 
 router.get('/',getGroups);
 router.post('/',createGroup);
+router.get('/:id',specificGroup);
+router.patch('/:id/name',updateGroup);
 
 
 
